52a97a9a58ccd87b2f2c367b3c5c974c277ccf15
0x11a41 <harikrishnamohan@proton.me>
2026-02-21 15:30:13 +0530

    vlc added to application list

--- configuration.nix
+++ configuration.nix
@@ -173,6 +173,8 @@
       godot
       wpsoffice
       android-studio
+      net-tools
+      vlc
     ];
   };
 
@@ -203,7 +205,8 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 443];
+    allowedTCPPorts = [ 80 443 6210 ];
+    allowedUDPPorts = [ 5353 ];
     allowedUDPPortRanges = [
       { from = 4000; to = 4007; }
       { from = 8000; to = 8010; }
